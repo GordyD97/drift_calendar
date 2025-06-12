35c1a2e7c1684a703e2ca976cdf99cdd2820d3d1
Gordon D <gordon.draine97@yahoo.com>
2025-06-11 23:43:14 -0400

    server syntax correction

--- server/server.cjs
+++ server/server.cjs
@@ -14,5 +14,18 @@ app.options('*', (req, res, next) => {
     res.sendStatus(200);
 });
 
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+    // res.header('Access-Control-Allow-Origin', '*');
+    // res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');  
+    // res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-requested-With');
+    // next();
+    console.log(`Request received: ${req.method} ${req.url}`);
+    next();
+});
+
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
